b80725fb78d2851ca557de326461573d663c2942
TikhonP <tikhon.petrishchev@gmail.com>
2026-03-13 17:55:49 +0300

    Fixed getUser endpoint

--- internal/endpoints/opensubsonicapi/handlers/user_management/get_user.go
+++ internal/endpoints/opensubsonicapi/handlers/user_management/get_user.go
@@ -1,7 +1,37 @@
 package usermanagement
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	osmodels "github.com/tikhonp/openswingsonic/internal/endpoints/opensubsonicapi/models"
+	"github.com/tikhonp/openswingsonic/internal/endpoints/opensubsonicapi/utils"
+)
 
 func (h *SystemHandler) GetUser(c echo.Context) error {
-	return nil
+	smUser, err := h.GetAuthedClient(c).User()
+	if err != nil {
+		return err
+	}
+
+	settings, err := h.GetAuthedClient(c).NotSettings()
+	if err != nil {
+		return err
+	}
+	scrobblingEnabled := settings.LastfmSessionKey != ""
+
+	return utils.RenderResponse(c, "user", osmodels.User{
+		Username:            smUser.Username,
+		ScrobblingEnabled:   scrobblingEnabled,
+		AdminRole:           false,
+		SettingsRole:        false,
+		DownloadRole:        true,
+		UploadRole:          false,
+		PlaylistRole:        false,
+		CoverArtRole:        true,
+		CommentRole:         false,
+		PodcastRole:         false,
+		StreamRole:          true,
+		JukeboxRole:         false,
+		ShareRole:           false,
+		VideoConversionRole: false,
+	})
 }
