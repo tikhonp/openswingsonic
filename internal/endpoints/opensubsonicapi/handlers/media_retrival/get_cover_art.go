bbfeb79cd7830e0f27c09d5a6ff42a18e1e82276
TikhonP <tikhon.petrishchev@gmail.com>
2026-04-07 18:48:37 +0300

    Trying to fix empty thumbnails in feishin

--- internal/endpoints/opensubsonicapi/handlers/media_retrival/get_cover_art.go
+++ internal/endpoints/opensubsonicapi/handlers/media_retrival/get_cover_art.go
@@ -3,6 +3,7 @@ package mediaretrival
 import (
 	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/tikhonp/openswingsonic/internal/middleware"
@@ -15,6 +16,15 @@ func (h *MediaRetrivalHandler) GetCoverArt(c echo.Context) error {
 		return middleware.RequiredParametrIsMissing
 	}
 	client := h.GetAuthedClient(c)
+
+	// if already valid id like de62d74ea26abc18.webp?pathhash=6a454f11d079fbdd
+	if strings.Contains(id, "webp?pathhash") {
+		contentType, imgReader, err := client.GetThumbnailByID(id)
+		if err != nil {
+			return err
+		}
+		return c.Stream(http.StatusOK, contentType, imgReader)
+	}
 
 	album, err := client.Album(id, 0)
 	if err == nil {
